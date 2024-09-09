95e6cdedbbc578eeb7c1f4dce4095024210c6842
Boban Kilvin <btrajcevski10@gmail.com>
2024-09-09 04:36:54 -0400

    Add trycatch to insertUser

--- src/db/queries.js
+++ src/db/queries.js
@@ -2,9 +2,14 @@ import { default as pool } from "./pool.js";
 
 export const insertUser = async (user) => {
   const { firstName, lastName, username, password } = user;
-  await pool.query(
-    `INSERT INTO account(first_name, last_name, username, password)
+  try {
+    await pool.query(
+      `INSERT INTO account(first_name, last_name, username, password)
     VALUES ($1, $2, $3, $4)`,
-    [firstName, lastName, username, password],
-  );
+      [firstName, lastName, username, password],
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
+
